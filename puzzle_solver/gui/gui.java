40af6f7acd0f30ca757fdc8528d8d88b9921e280
Kai Kitagawa-Jones <kaikitagawajones@gmail.com>
2021-10-05 00:00:28 +0200

    gui

--- puzzle_solver/gui/gui.java
+++ puzzle_solver/gui/gui.java
@@ -2,6 +2,8 @@ package puzzle_solver.gui;
 
 import java.awt.event.*;
 import javax.swing.*;
+import java.awt.*;
+// import java.util.Random;
 
 import puzzle_solver.solver.*;
 
@@ -20,33 +22,41 @@ public class gui {
 
 		JLabel widthLabel = new JLabel("Width:");
 		widthLabel.setBounds(0, height - 60, 60, 20);
-		JTextField widthField = new JTextField();
+		JTextField widthField = new JTextField("12");
 		widthField.setBounds(70, height - 60, 60, 20);
 
 		JLabel heightLabel = new JLabel("Height:");
 		heightLabel.setBounds(140, height - 60, 60, 20);
-		JTextField heightField = new JTextField();
+		JTextField heightField = new JTextField("5");
 		heightField.setBounds(140 + 70, height - 60, 60, 20);
 
 		JLabel lettersFieldLabel = new JLabel("Pentominos:");
 		lettersFieldLabel.setBounds(290, height - 60, 100, 20);
-		JTextField lettersField = new JTextField();
+		JTextField lettersField = new JTextField("X I Z T U V W Y L P N F");
 		lettersField.setBounds(400, height - 60, 300, 20);
 
 		JButton runButton = new JButton("RUN");
 		runButton.setBounds(710, height - 60, 80, 20);
 
+		Drawing canvas = new Drawing();
+		canvas.setSize(width, height - 60);
+		canvas.height = height - 60;
+		canvas.width = width;
+
 		runButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				// runButton.setText("WAIT");
+				// f.revalidate();
+
 				String letterString = lettersField.getText();
 				String widthString = widthField.getText();
 				String heightString = heightField.getText();
 
-				lettersField.setText("");
-				widthField.setText("");
-				heightField.setText("");
+				f.remove(canvas);
+				canvas.pieces = parse_and_run_input(letterString, widthString, heightString);
+				f.add(canvas);
 
-				parse_and_run_input(letterString, widthString, heightString);
+				// runButton.setText("RUN");
 			}
 		});
 
@@ -58,12 +68,14 @@ public class gui {
 		f.add(heightField);
 		f.add(runButton);
 
+		f.add(canvas);
+
 		f.setSize(800, 600);
 		f.setLayout(null);
 		f.setVisible(true);
 	}
 
-	private static void parse_and_run_input(String lettersString, String widthString, String heightString) {
+	private static int[][][] parse_and_run_input(String lettersString, String widthString, String heightString) {
 		char[] letters = new char[0];
 
 		for (int i = 0; i < lettersString.length(); i++) {
@@ -77,7 +89,7 @@ public class gui {
 		int heightInt = Integer.parseInt(heightString);
 
 		// if (!(width > 0 && height > 0 && letters.length > 0))
-		Solver.create_and_solve(widthInt, heightInt, letters);
+		return Solver.create_and_solve(widthInt, heightInt, letters);
 
 	}
 
@@ -90,5 +102,51 @@ public class gui {
 		newArray[newArray.length - 1] = element;
 
 		return newArray;
+	}
+}
+
+class Drawing extends Canvas {
+	public int[][][] pieces = new int[0][0][0];
+	public int width;
+	public int height;
+
+	public void paint(Graphics g) {
+		g.setColor(Color.LIGHT_GRAY);
+		g.fillRect(0, 0, width, height);
+
+		if (pieces.length != 0) {
+			// Random r = new Random();
+
+			int colSize = (width - 50) / pieces[0][0].length;
+			int rowSize = (height - 50) / pieces[0].length;
+
+			if (colSize < rowSize)
+				rowSize = colSize;
+
+			if (rowSize < colSize)
+				colSize = rowSize;
+
+			int xOffset = (width - colSize * pieces[0][0].length) / 2;
+			int yOffset = (height - rowSize * pieces[0].length) / 2;
+
+			for (int i = 0; i < pieces.length; i++) {
+				float colorMask = 1.0f / (float) pieces.length * i;
+
+				// Color color = Color.getHSBColor(r.nextFloat(), 1.0f, 0.5f);
+				Color color = Color.getHSBColor(colorMask, 0.5f + colorMask * 0.5f, 0.5f + colorMask * 0.5f);
+
+				for (int j = 0; j < pieces[0].length; j++) {
+					for (int k = 0; k < pieces[0][0].length; k++) {
+						if (pieces[i][j][k] == 1) {
+							g.setColor(color);
+							g.fillRect(xOffset + colSize * k, yOffset + rowSize * j, rowSize, colSize);
+							g.setColor(Color.lightGray);
+							g.drawRect(xOffset + colSize * k, yOffset + rowSize * j, rowSize, colSize);
+
+						}
+					}
+				}
+			}
+		}
 	}
 }
--- puzzle_solver/solver/Solver.java
+++ puzzle_solver/solver/Solver.java
@@ -3,12 +3,18 @@ package puzzle_solver.solver;
 import puzzle_solver.algorithm_x.*;
 import puzzle_solver.matrix_creator.*;
 // import puzzle_solver.utils.*;
+import puzzle_solver.utils.ArrayUtils;
 
 public class Solver {
 	// change these parameters for testing:
-	static final int WIDTH = 12;
-	static final int HEIGHT = 5;
-	static final char[] PIECES = { 'X', 'I', 'Z', 'T', 'U', 'V', 'W', 'Y', 'L', 'P', 'N', 'F' };
+	// static final int WIDTH = 10;
+	// static final int HEIGHT = 6;
+	// static final char[] PIECES = { 'X', 'I', 'Z', 'T', 'U', 'V', 'W', 'Y', 'L',
+	// 'P', 'N', 'F' };
+
+	static final int WIDTH = 5;
+	static final int HEIGHT = 3;
+	static final char[] PIECES = { 'X', 'U', 'U' };
 
 	public static void main(String[] args) {
 		create_and_solve(WIDTH, HEIGHT, PIECES);
@@ -19,16 +25,24 @@ public class Solver {
 	 * This method actually solves the puzzle
 	 */
 
-	public static void create_and_solve(int width, int height, char[] pieces) {
+	public static int[][][] create_and_solve(int width, int height, char[] pieces) {
+		if (width * height != pieces.length * 5)
+			return new int[0][0][0];
+
 		int[][] matrix = MatrixCreator.create(width, height, pieces);
 
 		int[] solutions = AlgorithmX.solve(matrix);
 
+		int[][][] solutions2D = new int[0][0][0];
+
 		for (int i = 0; i < solutions.length; i++) {
 			int[][] piece = MatrixCreator.get_pentomino_location_in_board(matrix[solutions[i]], pieces.length, width,
 					height);
+			solutions2D = ArrayUtils.add_element(solutions2D, piece);
 			MatrixCreator.print_board(piece);
 		}
+
+		return solutions2D;
 	}
 
 	/*
